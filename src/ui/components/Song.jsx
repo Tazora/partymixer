eccaf867cae91ff0135e8d192089c75c4fcd2b78
Sebastian Thon <sthon@hotmail.de>
2018-02-16 17:00:57 +0100

    Changed buttons

--- src/ui/components/Song.jsx
+++ src/ui/components/Song.jsx
@@ -7,7 +7,7 @@ function handleVote() {
 const Song = (props) => (
     <li key={props.index}>
         {props.wish.artist} - {props.wish.title} - Upvotes: {props.wish.votes.up} {props.wish.votes.up}
-        <a className="button is-active" onClick={handleVote()}><i className="far fa-thumbs-down"></i></a>
+        <a className="button is-active" onClick={this.handleVote()}><i className="far fa-thumbs-down"></i></a>
         <a className="button is-active"><i className="far fa-thumbs-up"></i></a>
     </li>
 )
